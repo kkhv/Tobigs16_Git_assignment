623be541df708727827a88531eb68a8646e1e169
kimkwonho <rlarnjsgh99@gmail.com>
2021-07-22 15:30:51 +0900

    문제 풀이

--- group4/player1/question1.py
+++ group4/player1/question1.py
@@ -5,7 +5,7 @@ def question():
     # 문제 : 리스트 안의 숫자들을 더해서 결과를 반환하겠습니다!
     target = [5, 10, -5, 6, 7, -1, 5, 1, 11, -5, 0, 1]
     sum = 0
-    for i in range(10):
+    for i in range(len(target)):
         sum += target[i]
 
     assert sum == 35 # 합이 35가 맞는지 측정합니다. 이 줄은 수정하지 말아 주세요!
